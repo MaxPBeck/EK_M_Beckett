4500b7913eaba45e0b21de65e50e941b6c1e03ed
MaxPBeck <mmm@gmail.com>
2024-11-15 11:42:03 +0100

    update

---
+++ assets/layout/footer.php
@@ -0,0 +1,55 @@
+</main>
+
+  <footer id="footer" class="footer dark-background">
+  
+    <div class="container footer-top">
+      <div class="row gy-4">
+        <div class="col-lg-3 col-md-6 footer-about">
+          <a href="index.php" class="d-flex align-items-center">
+            <span class="sitename">Max Beckett</span>
+          </a>
+          <div class="footer-contact pt-3">
+            <p>Ohmstraße 6A</p>
+            <p>Detmold, 32756</p>
+            <p class="mt-3"><strong>Phone:</strong> <span>05231 3038610</span></p>
+            <p><strong>Email:</strong> <span>maxbeckettdaa@gmail.com</span></p>
+          </div>
+        </div>
+
+        <div class="col-lg-2 col-md-3 footer-links">
+          <h4>Useful Links</h4>
+          <ul>
+            <li><i class="bi bi-chevron-right"></i> <a href="index.php">Home</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="about.php">About</a></li>
+            <li><i class="bi bi-chevron-right"></i> <a href="login.php">Login</a></li>
+          </ul>
+        </div>
+
+        
+        <div class="col-lg-4 col-md-12">
+          <h4>Follow Me</h4>
+          <div class="social-links d-flex">
+            <a href=""><i class="bi bi-twitter-x"></i></a>
+            <a href=""><i class="bi bi-facebook"></i></a>
+            <a href=""><i class="bi bi-instagram"></i></a>
+            <a href=""><i class="bi bi-linkedin"></i></a>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container copyright text-center mt-4">
+      <p>© <span>Copyright</span> <strong class="px-1 sitename">Max Beckett</strong> <span>All Rights Reserved</span></p>
+      <div class="credits">
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+
+  </footer>
+ 
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php require_once('./assets/layout/header.php'); ?> 
+
+    <!-- Hero Section -->
+    <section id="hero" class="hero section dark-background">
+
+      <!-- <img src="assets/img/hero-bg.jpg" alt="" data-aos="fade-in"> -->
+
+      <div id="hero-carousel" class="carousel carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
+
+        <div class="container position-relative">
+
+          <div class="carousel-item active">
+            <div class="carousel-container">
+              <h2>Welcome to Moderna</h2>
+              <p>Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+              <a href="#about" class="btn-get-started">Read More</a>
+            </div>
+          </div><!-- End Carousel Item -->
+
+          
+
+    <?php require_once('./assets/layout/footer.php'); ?> 
